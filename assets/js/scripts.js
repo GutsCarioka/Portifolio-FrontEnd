673620c1e543bf207c5af8c6c7f100e2842c435d
GutsCarioka <modestiagabriel@gmail.com>
2026-01-12 21:08:08 -0300

    Enhance form submission functionality; update styles for footer textarea and add success/error messages

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,5 +17,56 @@ class formSubmit {
     this.settings = settings;
     this.form = document.querySelector(settings.form);
     this.formButton = document.querySelector(settings.button);
+    if (this.form) {
+      this.url = this.form.getAttribute("action");
+    }
+    this.sendForm = this.sendForm.bind(this);
+  }
+  displaySucess() {
+    this.form.innerHTML = this.settings.sucess;
+  }
+  displayError() {
+    this.form.innerHTML = this.settings.error;
+  }
+  getFormObject() {
+    const formObject = {};
+    const fields = this.form.querySelectorAll("[name]");
+    fields.forEach((field) => {
+      formObject[field.getAttribute("name")] = field.value;
+    });
+    return formObject;
+  }
+  onSubmission(event) {
+    event.preventDefault();
+    event.target.disable = true;
+    event.target.innerText = "Enviando...";
+  }
+  async sendForm(event) {
+    try {
+      this.onSubmission(event);
+      await fetch(this.url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "applcation/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(this.getFormObject()),
+      });
+      this.displaySucess();
+    } catch {
+      this.displayError();
+      throw new Error("Erro ao enviar o formulário");
+    }
+  }
+  init() {
+    if (this.form) this.formButton.addEventListener("click", this.sendForm);
+    return this;
   }
 }
+const formSubmit = new FormSubmit({
+  form: "[data-form]",
+  button: "[data-button]",
+  sucess: "<h1 class = 'success'>Mensagem enviada com sucesso!</h1>",
+  error: "<h1 class = 'error'>Ocorreu um erro ao enviar a mensagem.</h1>",
+});
+formSubmit.init();
