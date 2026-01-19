c591d178923e573d7b1c1891ff393e486a63c29b
GutsCarioka <modestiagabriel@gmail.com>
2026-01-19 19:07:37 -0300

    Implement project details display; update Info.js to fetch project data based on URL parameters and modify infoProject.html structure for dynamic content rendering

--- assets/js/Info.js
+++ assets/js/Info.js
@@ -1,20 +1,50 @@
-// Captura todos os botões
-const botoes = document.querySelectorAll("button");
-const conteudo = document.getElementById("test");
+//Capturar o parametro da Url
+const params = new URLSearchParams(window.location.search);
+const projectId = params.get("project");
 
-// Adiciona evento de clique para cada botão
-botoes.forEach((botao) => {
-  botao.addEventListener("click", () => {
-    // Pega o ID do botão clicado
-    const idBotao = botao.id;
+//Base de dados dos Projetos
+const projects = {
+  refazendo_youtube: {
+    title: "Refazendo a Tela do Youtube",
+    description:
+      "Projeto desenvolvido durante estudos de desenvolvimento web, com foco em CSS e responsividade.",
+    image: "./assets/Images/Projetos/Replica Youtube/youtube.png",
+    github: "https://github.com/GutsCarioka/Refazendo_Tela_Youtube",
+  },
+  diabetes: {
+    title: "DeepLearning Project",
+    description:
+      "Projeto de Deep Learning em Python desenvolvido como trabalho final da disciplina de IA.",
+    image: "./assets/Images/Projetos/Diabetes.png",
+    github: "https://github.com/GutsCarioka/DeepLearning_Project",
+  },
 
-    // Altera o conteúdo com base no ID
-    if (idBotao === "btn1") {
-      conteudo.textContent = "Você clicou no Botão 1!";
-    } else if (idBotao === "btn2") {
-      conteudo.textContent = "Você clicou no Botão 2!";
-    } else if (idBotao === "btn3") {
-      conteudo.textContent = "Você clicou no Botão 3!";
-    }
-  });
-});
+  "yu-gi-oh": {
+    title: "Yu-Gi-Oh | Jo-ken-po Edition",
+    description:
+      "Jogo de Jokenpo criado para explicar conceitos de lógica de programação.",
+    image: "./assets/Images/Projetos/YU-GI-OH.png",
+  },
+
+  pokedex: {
+    title: "PokeDex",
+    description: "Pokedex com integração com a PokeAPI.",
+    image: "./assets/Images/Projetos/PokeDex.png",
+  },
+};
+
+//Se o projeto existir na base de dados, preencher as infromaçoes na página
+if (projects[projectId]) {
+  document.getElementById("project-title").textContent =
+    projects[projectId].title;
+
+  document.getElementById("project-description").textContent =
+    projects[projectId].description;
+
+  const img = document.getElementById("project-image");
+  img.src = projects[projectId].image;
+  img.alt = projects[projectId].title;
+} else {
+  document.getElementById("project-title").textContent =
+    "Projeto não encontrado";
+}
